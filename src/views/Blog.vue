2a7bd24da9c6ddb9247c4d2d6e9a35b632f126cd
edwincarr <edwincarrillo9@outlook.com>
2023-08-01 04:39:14 -0400

    Markdown to Vue Test

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,12 +1,16 @@
 <script setup>
 import Nav from '../components/Nav.vue';
 import Footer from '../components/Footer.vue';
+import Blog1 from '../assets/blogs/text.md'
+
+console.log(Blog1)
 </script>
 
 <template>
   <div>
     <Nav />
-      <div class="h-screen"></div>
+      <div class="h-screen" v-html="Blog1">
+      </div>
     <Footer />
   </div>
 </template>
