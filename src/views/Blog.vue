2f3aaa8d2e2ca400809b89eacc4168ea7fe1361a
edwincarr <edwincarrillo9@outlook.com>
2023-08-03 02:52:20 -0400

    Scrubbing md and shifting to branch

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,14 +1,13 @@
 <script setup>
 import Nav from '../components/Nav.vue';
 import Footer from '../components/Footer.vue';
-import Blog1 from '../assets/blogs/text.md'
+
 </script>
 
 <template>
   <div>
     <Nav />
       <div class="h-screen">
-        <Blog1 />
       </div>
     <Footer />
   </div>
