273f9afaf522d7064f7dc9fa6a1ba873e9a4422c
edwincarr <edwincarrillo9@outlook.com>
2023-08-05 10:06:09 -0400

    Blog-Image-functionality++ && dynamic body bg-color change

--- src/components/PostModal.vue
+++ src/components/PostModal.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref } from 'vue'
-import { StructuredText } from 'vue-datocms';
+import { ref, h } from 'vue'
+import { StructuredText, Image } from 'vue-datocms';
 
 const open = ref(false)
 
@@ -11,6 +11,19 @@ const closeModal = (event) => {
     open.value = false;
   }
 }
+
+const renderBlock = ({ record }) => {
+  switch (record.__typename) {
+    case 'ImageRecord':
+      return h(Image, {
+        data: record.image.responsiveImage,
+        class: 'w-fit'
+      });
+    default:
+      return null;
+  }
+};
+
 </script>
 
 <template>
@@ -25,21 +38,21 @@ const closeModal = (event) => {
     <p class="rounded-full p-3 bg-dark bg-opacity-30 dark:bg-light dark:bg-opacity-30 lg:text-2xl">{{ preview }}</p>
   </div>
 
-  <Teleport to="body">
     <div v-if="open" class="modal bg-black bg-opacity-20 py-10 " @click="closeModal">
-      <div class="px-10 lg:mx-[23%] lg:text-2xl max-w-fit bg-dark text-light z-[9999] rounded-2xl border border-light">
-        <h1 class="text-5xl mb-10 pt-10"><b>{{ title }}</b></h1>
-        <StructuredText :data="content" class="flex flex-col gap-4"/>
+      <div class="px-10 lg:mx-[23%] lg:text-2xl max-w-fit z-[9999] rounded-2xl dark:bg-dark bg-light">
+        <div class="md:flex items-center justify-evenly">
+          <h1 class="text-5xl mb-10 pt-10"><b class="text-splash">{{ title }}</b></h1>
+          <p class="text-xl max-md:mb-10">{{ posted }}</p>
+        </div>
+        <StructuredText :data="content" :renderBlock="renderBlock" class="flex flex-col gap-5 items-center"/>
         <button @click="open = false" class="my-14">Close</button>
       </div>
     </div>
-  </Teleport>
 </template>
 
 <style scoped>
 .modal {
   position: absolute;
   top: 0%;
-  /* background-color: aqua; */
 }
 </style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -11,7 +11,7 @@ import PostModal from '../components/PostModal.vue';
     <Nav />
     <div class="flex flex-col justify-center items-center my-20 gap-5">
       <h1 class="text-9xl "><b class="text-splash">Blog</b></h1>
-      <p class="text-2xl text-center">The one stop shop for my documented failures</p>
+      <p class="text-2xl text-center">The one stop shop for my documented failures.</p>
     </div>
       <div class=" flex flex-col items-center md:px-[10%] gap-10 min-h-[30rem]">
         <PostModal v-for="blog in store.blogs" :title="blog.title" :posted="blog.posted" :blurb="blog.blurb" :content="blog.content" :preview="blog.preview" :key="blog.id"/>
