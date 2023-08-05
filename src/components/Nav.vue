1731b3dbd2a13a8fbfbd99b66b9c6817251ed18a
edwincarr <edwincarrillo9@outlook.com>
2023-08-05 10:24:44 -0400

    Added bg-color transition

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -10,7 +10,7 @@ import { isDarkMode } from '../utils/LightSwitch';
       <RouterLink to='/blog' class='hover:text-splash group-hover:transition-colors group-hover:duration-500'>Blog</RouterLink>
       <RouterLink to='/about' class='hover:text-splash group-hover:transition-colors group-hover:duration-500'>About</RouterLink>
     </ul>
-    <button @click="toggleDarkMode" class="hover:text-splash transition duration-250 ">
+    <button @click="toggleDarkMode" class="hover:text-splash transition duration-200">
       <v-icon v-if='isDarkMode' name='md-lightmode' scale="1.4"/>
       <v-icon v-else='isDarkMode' name='md-darkmode' scale="1.4"/>
     </button>
