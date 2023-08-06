6d4378a2104f02316572481aca934cd5913e60c6
edwincarr <edwincarrillo9@outlook.com>
2023-08-06 19:20:48 -0400

    Added rough styled Interests

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -2,12 +2,14 @@
 import Nav from '../components/Nav.vue';
 import Footer from '../components/Footer.vue';
 import Me from '../components/Me.vue';
+import Interests from '../components/Interests.vue';
 </script>
 
 <template>
   <div>
     <Nav />
     <Me />
+    <Interests />
     <Footer />
   </div>
 </template>
